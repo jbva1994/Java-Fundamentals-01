80ac28627a63500480c2cddd17bd4a877e9ad3c4
jbva1994 <44219866+jbva1994@users.noreply.github.com>
2019-03-13 19:11:19 -0500

    CRUD Proveedor

--- SPORTSHOP/src/com/jbva/sportshop/controlador/ProveedorTrs.java
+++ SPORTSHOP/src/com/jbva/sportshop/controlador/ProveedorTrs.java
@@ -6,6 +6,7 @@ package com.jbva.sportshop.controlador;
 import com.jbva.sportshop.modelo.BdMemoria;
 import com.jbva.sportshop.modelo.Proveedor;
 
+
 /**
  * Clase que tendrá las operaciones de negocio relacionadas con el proveedor
  * 
@@ -17,9 +18,20 @@ public class ProveedorTrs implements InterfazCRUD {
 
 	@Override
 	public String insertar(Object registro) {
+		boolean banIng = false;
 		if (registro != null) {
-			BdMemoria.proveedores[0] = (Proveedor) registro;
-			return "\nProveedor guardado correctamente";
+			for (int i = 0; i < BdMemoria.proveedores.length; i++) {
+				if (BdMemoria.proveedores[i] == null) {
+					BdMemoria.proveedores[i] = (Proveedor) registro;
+					banIng = true;
+					break;
+				}
+			}
+			if (banIng) { // la bandera automaticamente esta en verdadero
+				return "Proveedor guardado correctamente";
+			} else {
+				return "El tamaño máximo permitido es de 4 proveedores para la versión demo";
+			}
 		} else {
 			return "Debe llenar todos los datos!!!!";
 		}
@@ -27,14 +39,52 @@ public class ProveedorTrs implements InterfazCRUD {
 
 	@Override
 	public String actualizar(int id, Object registro) {
-		// TODO Auto-generated method stub
-		return null;
+		/**************************************************
+		 * BLOQUE PARA BUSCAR
+		 **************************************************/
+		int posEnc = 0;
+		boolean banEnc = false;
+		for (Proveedor tipPro : BdMemoria.proveedores) {
+			if (tipPro != null && tipPro.getIdProveedor() == id) {
+				// Recuperarme la posición donde le encontre
+				banEnc = true;
+				break;
+			}
+			posEnc++;
+		}
+		/**************************************************/
+		
+		if (banEnc) {
+			BdMemoria.proveedores[posEnc] = (Proveedor) registro;
+			return "Registro actualizado correctamente";
+		} else {
+			return "No se encontró registro";
+		}
 	}
 
 	@Override
 	public String eliminar(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		/**************************************************
+		 * BLOQUE PARA BUSCAR
+		 **************************************************/
+		int posEnc = 0;
+		boolean banEnc = false;
+		for (Proveedor tipPro : BdMemoria.proveedores) {
+			if (tipPro != null && tipPro.getIdProveedor() == id) {
+				// Recuperarme la posición donde le encontre
+				banEnc = true;
+				break;
+			}
+			posEnc++;
+		}
+		/**************************************************/
+		
+		if (banEnc) {
+			BdMemoria.proveedores[posEnc] = null;
+			return "Registro eliminado correctamente";
+		} else {
+			return "No se encontró registro";
+		}
 	}
 
 	@Override
--- SPORTSHOP/src/com/jbva/sportshop/vista/FrmProveedor.java
+++ SPORTSHOP/src/com/jbva/sportshop/vista/FrmProveedor.java
@@ -5,7 +5,10 @@ package com.jbva.sportshop.vista;
 
 import com.jbva.sportshop.controlador.ProveedorTrs;
 import com.jbva.sportshop.modelo.Proveedor;
+import com.jbva.sportshop.modelo.Usuario;
 import com.jbva.sportshop.util.UtilLectura;
+
+
 
 /**
  * Clase que representa el formulario del Proveedor
@@ -14,10 +17,9 @@ import com.jbva.sportshop.util.UtilLectura;
  * 5 mar. 2019 - 22:57:24
  */
 public class FrmProveedor {
+	private Usuario usuarioSesion;
 	
-	/**
-	 * 
-	 */
+	// Constructor
 	public FrmProveedor() {
 		crearMenuProveedor();
 	}
@@ -28,6 +30,13 @@ public class FrmProveedor {
 	private void crearMenuProveedor() {
 		int opcion = 0;
 		ProveedorTrs adminProveedor = new ProveedorTrs();
+		Object[] listaProveedores = null;
+		String mensaje = null;
+		//Proveedor proveedor = new Proveedor();
+		Proveedor proveedor = null;
+		String nombreProv = null;
+		String descripcionProv = null;
+		
 		do {
 			System.out.println("\n\n");
 			System.out.println("**************************************");
@@ -43,40 +52,79 @@ public class FrmProveedor {
 			// Sentencias de decision - switch
 			switch (opcion) { // solo entero o cadena
 			case 1:
-				//System.out.println(adminProveedor.listar());
+				listaProveedores = adminProveedor.listar();
+				for (Object tipProv : listaProveedores) {
+					System.out.println(tipProv);
+				}
 				break;
 			case 2:
 				//1.Recuperando los valores
-				System.out.println("\nIngresar los datos del Proveedor");
+				System.out.println("Ingresar los datos del Proveedor");
 				System.out.print("Id Proveedor:");
 				int idProv = Integer.parseInt(UtilLectura.leerDesdeTeclado());
 				System.out.print("Nombre Proveedor:");
-				String nombreProv = UtilLectura.leerDesdeTeclado();
+				nombreProv = UtilLectura.leerDesdeTeclado();
 				System.out.print("Descripción Proveedor:");
-				String descripcionProv = UtilLectura.leerDesdeTeclado();
+				descripcionProv = UtilLectura.leerDesdeTeclado();
 				
 				//2. Crear un registro (objeto de Proveedor)
-				Proveedor proveedor = new Proveedor();
+				proveedor = new Proveedor();
 				proveedor.setIdProveedor(idProv);
 				proveedor.setNombreProv(nombreProv);
 				proveedor.setDescripcionProv(descripcionProv);
 				
 				//3. Llamar al controlador (el tiene las operaciones)
-				String mensaje = adminProveedor.insertar(proveedor);
+				mensaje = adminProveedor.insertar(proveedor);
 				
 				//4.Procesar la información
 				System.out.println(mensaje);
+				
 				break;
+				
 			case 3:
+				// 1. A que registro le van actualizar
+				System.out.print("Cuál registro requiere actualizar, ingrese el id:");
+				int idProvAct = Integer.parseInt(UtilLectura.leerDesdeTeclado());
+
+				// 2. Recuperando los valores menos su id
+				System.out.println("Ingresar los datos");
+				System.out.print("Nombre:");
+				nombreProv = UtilLectura.leerDesdeTeclado();
+				System.out.print("Descripción:");
+				descripcionProv = UtilLectura.leerDesdeTeclado();
+
+				// 3. Crear un registro para actualizar (objeto de TipoProducto)
+				proveedor = new Proveedor();
+				proveedor.setIdProveedor(idProvAct);
+				proveedor.setNombreProv(nombreProv);
+				proveedor.setDescripcionProv(descripcionProv);
+
+				// 4. Llamar al controlador (el tiene las operaciones)
+				mensaje = adminProveedor.actualizar(idProvAct, proveedor);
+
+				// 5. Procesar la informacion
+				System.out.println(mensaje);
 				break;
+				
 			case 4:
+				// 1.Mostramos todo lo que tenemos
+				System.out.println("¿Cúal registro se requiere eliminar?");
+				listaProveedores = adminProveedor.listar();
+				for (Object tipProv : listaProveedores) {
+					System.out.println(tipProv);
+				}
+				// 2.Recuperamos el identificador del tipo de producto a eliminar
+				System.out.print("Ingrese el id:");
+				int idProvEli = Integer.parseInt(UtilLectura.leerDesdeTeclado());
+
+				// 3.Llamar al controlar para eliminar
+				mensaje = adminProveedor.eliminar(idProvEli);
+				System.out.println(mensaje);
 				break;
+				
 			case 5:
 				FrmMenuPrincipal frmMenuPrincipal = new FrmMenuPrincipal();
-				break;
-			case 6:
-				System.exit(0); // Acabar el programa
-
+				
 			default:
 				System.err.println("Opción incorrecta!!!!");
 				break;
@@ -85,5 +133,7 @@ public class FrmProveedor {
 		} while (opcion != 5);
 		
 	}
-
+		
 }
+
+
